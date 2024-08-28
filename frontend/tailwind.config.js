c66325fea80a22b95644798f099a7b6c82c77d92
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-08-28 10:04:08 +0200

    created the header section

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,13 @@
+import flowbite from 'flowbite-react/tailwind';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    // Include paths to your files where Tailwind CSS classes are used
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust this according to your project structure
+    flowbite.content(),
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  plugins: [
+    flowbite.plugin(),
+  ],
+};
