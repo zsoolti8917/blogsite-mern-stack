2be27f309252e6d378ff1b8125a7044cf3720719
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-08-28 12:59:41 +0200

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -17,3 +17,5 @@ app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
     }
 );
+
+app.use('/api/user', userRoutes);
