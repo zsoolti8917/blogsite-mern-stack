d65deca15e57569f57aa72494c17972da7116294
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-08-28 11:29:49 +0200

    connect to mongdb Cloud

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+    }   
+).catch((error) => {
+    console.log('Error:', error);
+    }
+);
+const app = express()
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
