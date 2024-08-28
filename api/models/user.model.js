c973c5e5a6a8b813d70566d8a49e40ba24fee259
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-08-28 11:38:50 +0200

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    }
+        
+    }, {timestamps: true}
+
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
