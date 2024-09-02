f624513388d4da17bb6c0bf319692c1a9fad603c
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-09-02 16:57:43 +0200

    header component with user data

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Navbar, TextInput, Button } from 'flowbite-react'
+import { Navbar, TextInput, Button, Dropdown, Avatar, DropdownDivider } from 'flowbite-react'
 import { Link, useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
 import {FaMoon, FaSun} from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
   const path = useLocation().pathname
+  const { currentUser } = useSelector(state => state.user)
   return (
     <Navbar className='border-b-2'>
       <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -25,11 +27,36 @@ const Header = () => {
           <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
             <FaMoon/>
           </Button>
-          <Link to='/sign-in'>
-            <Button className='' gradientDuoTone='purpleToBlue'>
-              Sign In
-            </Button>
-          </Link>
+          {currentUser ? (
+            <Dropdown arrowIcon={false} inline label= {
+              <Avatar alt='user' img={currentUser.profilePicture} rounded/>
+            }>
+              <Dropdown.Header>
+                <span className='block text-sm'>@{currentUser.username}</span>
+                <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+
+              </Dropdown.Header>
+              <Link to={'/dashboard?tab=profile'}>
+                <Dropdown.Item>
+                  Profile
+                </Dropdown.Item>
+              </Link>
+              <DropdownDivider />
+              
+
+                <Dropdown.Item>
+                  Sign Out
+                </Dropdown.Item>
+            </Dropdown>
+          ) : (
+<Link to='/sign-in'>
+<Button className='' gradientDuoTone='purpleToBlue'>
+  Sign In
+</Button>
+</Link>
+          )
+          }
+          
           <Navbar.Toggle className='w-12 h-10'/>
 
         </div>
